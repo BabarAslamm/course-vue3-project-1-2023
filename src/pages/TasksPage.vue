8cb74326257c1f96c45a6136005ac98c978b0c89
babar <babar@bitsoftsol.com>
2024-01-18 16:38:39 +0500

    completed and uncompleted tasks topic

--- src/pages/TasksPage.vue
+++ src/pages/TasksPage.vue
@@ -8,13 +8,14 @@
                         <input type="text" class="form-control form-control-lg padding-right-lg"
                             placeholder="+ Add new task. Press enter to save." />
                     </div>
-                    <!-- List of tasks -->
-                    <div class="card mt-2">
-                        <ul class="list-group list-group-flush">
-                            <!-- <Task v-for="task in tasks" :task="task" :key="task.id"  /> -->
-                            <Task v-for="task in tasks" :task="task" :key="task.id" />
-                        </ul>
-                    </div>
+                    <!-- List of uncompleted tasks -->
+                    <Tasks :tasks="uncompletedTasks" />
+
+                    <!-- Show toggle button -->
+
+                    <!-- List of completed tasks -->
+                    <Tasks :tasks="completedTask" />
+                    
                 </div>
             </div>
         </div>
@@ -23,15 +24,17 @@
 
 
 <script setup>
-import { onMounted, ref} from "vue";
+import { onMounted, ref, computed } from "vue";
 import { allTasks } from "../http/task-api";
-import Task from "../components/tasks/Task.vue";
+import Tasks from "../components/tasks/Tasks.vue";
 
 const tasks = ref([])
 
 onMounted (async () => {
   const {data} = await allTasks()
   tasks.value  = data.data
-
 })
+
+const uncompletedTasks = computed(()=> tasks.value.filter(task => !task.is_completed))
+const completedTask = computed(()=> tasks.value.filter(task => task.is_completed))
 </script>
